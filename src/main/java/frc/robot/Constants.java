11aeecbb24a8037e579811674653f750dfd26e35
Jordan Powers <jordanpowers1227@gmail.com>
2024-02-07 20:27:21 -0800

    Update swerve module CAN ids

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -34,14 +34,14 @@ public class Constants {
         }
     }
 
-    public static final CANAddress TURN_LEFT_FRONT = new CANAddress(13);
-    public static final CANAddress TURN_LEFT_REAR = new CANAddress(11);
+    public static final CANAddress TURN_LEFT_FRONT = new CANAddress(11);
+    public static final CANAddress TURN_LEFT_REAR = new CANAddress(13);
     public static final CANAddress TURN_RIGHT_FRONT = new CANAddress(5);
     public static final CANAddress TURN_RIGHT_REAR = new CANAddress(12);
-    public static final CANAddress DRIVE_LEFT_FRONT = new CANAddress(3);
-    public static final CANAddress DRIVE_LEFT_REAR = new CANAddress(1);
-    public static final CANAddress DRIVE_RIGHT_FRONT = new CANAddress(4);
-    public static final CANAddress DRIVE_RIGHT_REAR = new CANAddress(2);
+    public static final CANAddress DRIVE_LEFT_FRONT = new CANAddress(2);
+    public static final CANAddress DRIVE_LEFT_REAR = new CANAddress(3);
+    public static final CANAddress DRIVE_RIGHT_FRONT = new CANAddress(1);
+    public static final CANAddress DRIVE_RIGHT_REAR = new CANAddress(4);
 
     public static final HIDPort DRIVE_F310 = new HIDPort(0);
     public static final HIDPort JOYSTICK = new HIDPort(2);
