b43d47dacf8bf2521c0682768616c371c68cae9e
Jordan Powers <jordanpowers1227@gmail.com>
2024-02-19 14:24:56 -0800

    Add missing call to PIDTuner.pollAllStateValues()
    The PIDTuner class needs this called periodically so it can update the
    state values for all PID tuner instances in the NetworkTables.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,7 @@
 
 package frc.robot;
 
+import com.momentum4999.motune.PIDTuner;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -21,6 +22,8 @@ public class Robot extends TimedRobot {
     @Override
     public void robotPeriodic() {
         CommandScheduler.getInstance().run();
+
+        PIDTuner.pollAllStateValues();
     }
 
     @Override
