3fae8745bef68c79f88389eafaafec1857c7ada2
Momentum Robotics <momentumrobotics@gmail.com>
2024-03-16 12:27:39 -0700

    Use flywheel velocity instead of current sense for intake source

--- src/main/java/frc/robot/command/IntakeSourceCommand.java
+++ src/main/java/frc/robot/command/IntakeSourceCommand.java
@@ -9,17 +9,10 @@ import frc.robot.util.MoPrefs;
 public class IntakeSourceCommand extends Command {
     private final ShooterSubsystem shooter;
 
-    private final Timer currentSenseTimer = new Timer();
-
     public IntakeSourceCommand(ShooterSubsystem shooter) {
         this.shooter = shooter;
 
         addRequirements(shooter);
-    }
-
-    @Override
-    public void initialize() {
-        currentSenseTimer.restart();
     }
 
     @Override
@@ -30,11 +23,6 @@ public class IntakeSourceCommand extends Command {
 
     @Override
     public boolean isFinished() {
-        if (shooter.getRollerCurrent().gte(MoPrefs.handoffCurrentCutoff.get())) {
-            return currentSenseTimer.hasElapsed(MoPrefs.handoffTimeCutoff.get().in(Units.Seconds));
-        } else {
-            currentSenseTimer.restart();
-            return false;
-        }
+        return Math.abs(shooter.getAvgFlywheelVelocity().in(Units.MetersPerSecond)) > MoPrefs.backoffZeroTolerance.get();
     }
 }
